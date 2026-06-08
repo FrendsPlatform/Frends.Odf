16231ad210eb210eee7247bee23b5025a7b5db1c
gabri <gabrielfdearden@gmail.com>
2026-06-08 12:05:11 +0300

    Task initialization and Definition implementation

---
+++ Frends.Odf.WriteSpreadsheet/Frends.Odf.WriteSpreadsheet/Definitions/Input.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json.Linq;
+
+namespace Frends.Odf.WriteSpreadsheet.Definitions;
+
+/// <summary>
+/// Action to take if the destination file already exists.
+/// </summary>
+public enum ActionOnExistingFile
+{
+    /// <summary>
+    /// Replaces the existing file.
+    /// </summary>
+    Overwrite,
+
+    /// <summary>
+    /// Throws an error if the file already exists.
+    /// </summary>
+    Throw,
+}
+
+/// <summary>
+/// Essential parameters.
+/// </summary>
+public class Input
+{
+    /// <summary>
+    /// JSON data array to be written into the .ods file as rows and columns.
+    /// </summary>
+    /// <example>[ { "Name": "John", "Age": 40 }, { "Name": "Jane", "Age": 30 } ]</example>
+    [Required]
+    [DefaultValue("")]
+    public JToken Payload { get; set; }
+
+    /// <summary>
+    /// Full path of the destination for the new .ods file.
+    /// </summary>
+    /// <example>c:\temp\foo.ods</example>
+    [Required]
+    [DefaultValue("")]
+    public string FilePath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Defines how the file write should work if a file with the new name already exists.
+    /// </summary>
+    /// <example>ActionOnExistingFile.Throw</example>
+    [DefaultValue(ActionOnExistingFile.Throw)]
+    public ActionOnExistingFile ActionOnExistingFile { get; set; } = ActionOnExistingFile.Throw;
+}
